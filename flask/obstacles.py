bd35f72f0fc2a6465e07875f1ee35c72ff9354a3
natalia <ewewraw@gmail.com>
2023-10-06 16:16:35 +0200

    added get obstacles for polyline method

--- flask/obstacles.py
+++ flask/obstacles.py
@@ -2,13 +2,32 @@ import boto3
 from boto3.dynamodb.conditions import Attr
 from decimal import Decimal
 
+# TODO: optimize -> move to the server file and make it global
+db = boto3.resource('dynamodb', region_name='eu-central-1')
+obstacles_table = db.Table('Obstacles')
+
+
+def getObstaclesForPolyline(polyline):
+    # TODO: optimize
+    filter_expression = getFilterExpression(polyline[0])
+    for coordinate in polyline:
+        filter_expression = filter_expression | getFilterExpression(coordinate)
+
+    obstacles_on_the_way = obstacles_table.scan(
+        FilterExpression=filter_expression
+    )['Items']
+
+    print(obstacles_on_the_way)
+
+    return obstacles_on_the_way
+
 
 # DO we integrate Google map in the back or front?
 def getAllObstaclesOnTheWay(start, end):
     db = boto3.resource('dynamodb', region_name='eu-central-1')
     obstacles_table = db.Table('Obstacles')
     obstacles_on_the_way = obstacles_table.scan(
-        FilterExpression=Attr("obs_coordinate_long").gte(Decimal('6.1')) & Attr("obs_coordinate_long").lte(Decimal('6.8'))
+        FilterExpression=Attr("obs_coordinate_long").gte(Decimal(start.split['1']) + Decimal('0.00002'))) & Attr("obs_coordinate_long").lte(Decimal(start.split['1']) + Decimal('0.00002'))
     )['Items']
 
     return obstacles_on_the_way
@@ -24,24 +43,8 @@ def postObstacle():
     tTable = db.Table('Obstacles')
 
 
-# def getFeedbackForRoute(start, end):
-#     db = boto3.resource('dynamodb', region_name='eu-central-1')
-#     tTable = db.Table('Obstacles')
-#     print(tTable.scan()['Items'])
-#
-# def getFeedbackForRoute(start, end):
-#     db = boto3.resource('dynamodb', region_name='eu-central-1')
-#     tTable = db.Table('Obstacles')
-#     print(tTable.scan()['Items'])
-
-def is_point_inside_line_segment(start, end, point):
-    # Calculate the equation of the line passing through start and end points
-    line_equation = lambda x: (end[1] - start[1]) * (x - start[0]) / (end[0] - start[0]) + start[1]
-
-    # Check if the point lies within the bounding box of the line segment
-    if (start[0] <= point[0] <= end[0] or start[0] >= point[0] >= end[0]) and \
-            (start[1] <= point[1] <= end[1] or start[1] >= point[1] >= end[1]):
-        # Check if the point lies on or below the line
-        if point[1] <= line_equation(point[0]):
-            return True
-    return False
+def getFilterExpression(coordinate):
+    return Attr("obs_coordinate_long").gte(Decimal(coordinate.split['1']) + Decimal('0.00002')) \
+           & Attr("obs_coordinate_long").lte(Decimal(coordinate.split['1']) + Decimal('0.00002')) \
+           & Attr("obs_coordinate_lat").gte(Decimal(coordinate.split['0']) + Decimal('0.00002')) \
+           & Attr("obs_coordinate_lat").lte(Decimal(coordinate.split['0']) + Decimal('0.00002')) \
