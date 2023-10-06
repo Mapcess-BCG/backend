2cd0f3e9bc5f7ae691c86ffeb38df9e55fd6e266
natalia <ewewraw@gmail.com>
2023-10-06 15:37:02 +0200

    added basic obstacles get method

--- flask/obstacles.py
+++ flask/obstacles.py
@@ -1,14 +1,47 @@
 import boto3
+from boto3.dynamodb.conditions import Attr
+from decimal import Decimal
+
 
 # DO we integrate Google map in the back or front?
+def getAllObstaclesOnTheWay(start, end):
+    db = boto3.resource('dynamodb', region_name='eu-central-1')
+    obstacles_table = db.Table('Obstacles')
+    obstacles_on_the_way = obstacles_table.scan(
+        FilterExpression=Attr("obs_coordinate_long").gte(Decimal('6.1')) & Attr("obs_coordinate_long").lte(Decimal('6.8'))
+    )['Items']
+
+    return obstacles_on_the_way
+
 
 def getAllObstacles():
-    dynamodb = boto3.client('dynamodb', region_name='eu-central-1')
-    # s3 = boto3.resource('s3',
-    #                     aws_access_key_id=ACCESS_ID,
-    #                     aws_secret_access_key=ACCESS_KEY)
+    db = boto3.resource('dynamodb', region_name='eu-central-1')
+    tTable = db.Table('Obstacles')
+    print(tTable.scan()['Items'])
 
-    # Example: List all tables
-    response = dynamodb.list_tables()
-    print("Tables in DynamoDB:", response['TableNames'])
+def postObstacle():
+    db = boto3.resource('dynamodb', region_name='eu-central-1')
+    tTable = db.Table('Obstacles')
 
+
+# def getFeedbackForRoute(start, end):
+#     db = boto3.resource('dynamodb', region_name='eu-central-1')
+#     tTable = db.Table('Obstacles')
+#     print(tTable.scan()['Items'])
+#
+# def getFeedbackForRoute(start, end):
+#     db = boto3.resource('dynamodb', region_name='eu-central-1')
+#     tTable = db.Table('Obstacles')
+#     print(tTable.scan()['Items'])
+
+def is_point_inside_line_segment(start, end, point):
+    # Calculate the equation of the line passing through start and end points
+    line_equation = lambda x: (end[1] - start[1]) * (x - start[0]) / (end[0] - start[0]) + start[1]
+
+    # Check if the point lies within the bounding box of the line segment
+    if (start[0] <= point[0] <= end[0] or start[0] >= point[0] >= end[0]) and \
+            (start[1] <= point[1] <= end[1] or start[1] >= point[1] >= end[1]):
+        # Check if the point lies on or below the line
+        if point[1] <= line_equation(point[0]):
+            return True
+    return False
--- flask/server.py
+++ flask/server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-from obstacles import getAllObstacles
+
+from obstacles import getAllObstaclesOnTheWay, getAllObstacles
 
 app = Flask(__name__)
 
@@ -16,6 +17,19 @@ def todo():
     except:
         return "Server not available"
     return "Hello from the MongoDB client!\n"
+
+
+# start and end parameters are coordinates in form "lat:lon"
+@app.route('/obstacles', methods=['GET'])
+def getObstacles():
+    start = request.args.get('start', type=str)
+    end = request.args.get('end', type=str)
+
+    return jsonify({'obstacles': getAllObstaclesOnTheWay(start, end)})
+
+
+
+
 
 
 if __name__ == "__main__":
