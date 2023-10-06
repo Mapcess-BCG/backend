a9eeac4c2408f6b17f6fde6e1dd0975a5d2ee813
MoritzHaw <moritz.hawener@gmail.com>
2023-10-06 17:27:24 +0200

    added feedback file

---
+++ flask/feedback.py
@@ -0,0 +1,9 @@
+import boto3
+from boto3.dynamodb.conditions import Attr
+
+# get feedback along the route
+def getFeedbackAlongTheRoute(polylines):
+    db = boto3.resource('dynamodb', region_name='eu-central-1')
+    feedback_table = db.Table('Feedback')
+    feedback_along_route = feedback_table.scan()['Items']
+    return feedback_along_route
