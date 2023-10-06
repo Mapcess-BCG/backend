1851ab7cb2bd790c604160e4d37b2526379001a5
Magdalena Fuchs <magdalena-fuchs@hotmail.com>
2023-10-06 17:14:40 +0200

    Extract alternate routes

--- flask/get_directions.py
+++ flask/get_directions.py
@@ -1,7 +1,6 @@
 import requests
 import polyline
 from flask import Flask, request, jsonify
-# from map_utils import is_location_on_polyline
 
 GOOGLE_DIRECTIONS_API_URL = "https://maps.googleapis.com/maps/api/directions/json"
 GOOGLE_API_KEY = "AIzaSyAWEPohy9CdHpz6j8-_zLDRsSWoDI9b2YU"
@@ -29,16 +28,23 @@ def fetch_directions(origin, destination):
     
     if api_response.status_code == 200:
         # Extract polylines
+        # TODO: Handle alternate routes
         steps_list = api_response.json()['routes'][0]['legs'][0]['steps']
-        polylines = [steps_list[i]["polyline"]["points"] for i in range(0, len(steps_list))]
-        # return get_elevation(steps_list), polylines
         polylines = [polyline.decode(steps_list[i]["polyline"]["points"]) for i in range(0, len(steps_list))]
 
-        return polylines
+        # return len(polylines[0]), get_elevation(steps_list)
+        return api_response.json()
     else:
         return {'error': 'Failed to fetch directions'}, 500
     
-# def get_elevation(steps_list):
-#     for step in steps_list:
-        
-#     api_response = requests.get(GOOGLE_DIRECTIONS_API_URL, params=params)
+def get_elevation(steps_list):
+    for step in steps_list:
+        # Get start and end coordinates
+        start_lat = step["start_location"]["lat"]
+        start_lng = step["start_location"]["lng"]
+        end_lat = step["end_location"]["lat"]
+        end_lng = step["end_location"]["lng"]
+
+        # Get distance in meters
+        distance = step["distance"]["value"]
+        # api_response = requests.get(GOOGLE_DIRECTIONS_API_URL, params=params)
